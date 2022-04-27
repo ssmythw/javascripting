bd57fb57951f05465dd232278b06c3979e007a21
Scott <ssmythw@outlook.com>
2022-04-27 15:02:37 -0400

    first commit

---
+++ function-arguments.j
@@ -0,0 +1,5 @@
+const math = function(x,y,z){
+return x+y*z;
+}
+
+console.log(math(53,61,67));
---
+++ variables.js
@@ -0,0 +1,4 @@
+// make a variable called example and set it to 'some string'
+
+const example = 'some string';
+console.log(example);
